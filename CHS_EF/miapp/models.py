9c3d7c7f5fb42bde17f777eab819a035b35640c9
Anselmo Huancas <junior.huancas.leuyacc@gmail.com>
2022-08-03 17:56:38 -0500

    EF Parte02 - Producto - Huancas

--- CHS_EF/miapp/models.py
+++ CHS_EF/miapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+	codigo = models.TextField()
+	nombre = models.TextField()
+	precio_compra = models.TextField()
+	precio_venta = models.TextField()
+	Fecha_compra = models.TextField()
+	Fecha_registro = models.DateTimeField(auto_now_add=True)
+	estado =  models.TextField(max_length=1)
--- CHS_EF/miapp/views.py
+++ CHS_EF/miapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponse
+from miapp.models import Producto
 
 # Create your views here.
-
 def saludo(request):
     mensaje = "Bienvenidos a nuestro examen Final, Aprobados 2022-I"
     return render(request,'inicio.html',
@@ -16,7 +16,20 @@ def alumnnos(request):
         'titulo': 'LOS INTEGRANTES SON: '})
 
 def crear_productos(request):
-    return render(request, 'Crearproductos.html')
+    producto = Producto(
+	codigo = "05",
+	nombre = "libro",
+	precio_compra = "5.00",
+	precio_venta = "6.00",
+    Fecha_compra = "01/08/2022",
+	estado = "A",
+	)
+    producto.save()
+    return render(request, 'Crearproductos.html',
+        {'producto' : producto})
 
 def crear_curso(request):
     return render(request, 'Crearcursos.html')
+
+	
+
